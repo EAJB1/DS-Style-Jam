8c5c2f39629b8fa1b644ef4d843b6d2176eab89d
EAJB1 <edbuckett11@gmail.com>
2024-09-15 13:56:26 +0100

    Grid can spawn a specified number of tiles

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] GameObject mud, grass;
+    [SerializeField] GameObject mud, nut, grass;
 
     [Space]
 
-    [SerializeField] int size;
+    [SerializeField] int size, spawnCount;
     [SerializeField] float padding;
 
     [Space]
 
     public Transform selectedTile;
 
+    bool[] canSpawn;
+
     void Start()
     {
-        GenerateGridLayer(mud);
-        GenerateGridLayer(grass);
+        InitGrid();
     }
 
-    void GenerateGridLayer(GameObject tile)
+    void InitGrid()
+    {
+        GenerateGridLayer(mud, false);
+
+        GenerateRandomPositions();
+        GenerateGridLayer(nut, true);
+
+        //GenerateGridLayer(grass, false);
+    }
+
+    void GenerateGridLayer(GameObject tile, bool isNut)
     {
         Vector2 offset = (((Vector2.one * size) / 2f) + (Vector2.one * (size * padding)) / 2f) - new Vector2(0.5f, 0.5f);
         Debug.Log(offset);
+
+        int index = 0;
 
         for (int y = 0; y < size; y++)
         {
@@ -32,9 +45,33 @@ public class GridManager : MonoBehaviour
             {
                 Vector2 position = new Vector2(x, y) * (1f + padding) - offset;
 
+                if (isNut && !canSpawn[index])
+                {
+                    continue;
+                }
+
                 Tile currentTile = Instantiate(tile, position, Quaternion.identity).GetComponent<Tile>();
                 currentTile.transform.parent = transform;
                 currentTile.coordinates = new Vector2(x, y);
+
+                index++;
+            }
+        }
+    }
+
+    void GenerateRandomPositions()
+    {
+        canSpawn = new bool[size * size];
+        int count = spawnCount;
+
+        for (int i = 0; i < canSpawn.Length; i++)
+        {
+            int rnd = Random.Range(0, 1);
+
+            if (count > 0 /*&& rnd == 0*/)
+            {
+                canSpawn[i] = true;
+                count--;
             }
         }
     }
